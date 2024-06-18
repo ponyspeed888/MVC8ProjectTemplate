a3b7e6814548dcc98a54271008e03d92af32969a
jy <jy@TAICHI>
2024-06-18 19:02:18 +0800

    Add project files.

---
+++ MVC8ProjectTemplate/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVC8ProjectTemplate.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
